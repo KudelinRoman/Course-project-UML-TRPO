6587a9349e7a47eabb99896658b4c11293e7b6e6
unknown <roman.kudelin.00@gmail.com>
2019-05-03 23:56:12 +0300

    first commit

---
+++ Course Project/Course Project/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Course_Project
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.novoe = new System.Windows.Forms.ToolStripButton();
+			this.toolStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.AllowDrop = true;
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoe});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+			this.toolStrip1.TabIndex = 0;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// novoe
+			// 
+			this.novoe.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.novoe.Image = ((System.Drawing.Image)(resources.GetObject("novoe.Image")));
+			this.novoe.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.novoe.Name = "novoe";
+			this.novoe.Size = new System.Drawing.Size(23, 22);
+			this.novoe.Text = "&Создать";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.toolStrip1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton novoe;
+	}
+}
+
