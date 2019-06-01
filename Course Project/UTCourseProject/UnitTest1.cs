f84975478a92dae6727a1914a5fbd12ed888524e
KudelinRoman <roman.kuddelin.00@gmail.com>
2019-06-02 00:35:03 +0300

    Пространства имен

---
+++ Course Project/UTCourseProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibCourseProject;
+
+namespace UTCourseProject
+{
+	[TestClass]
+	public class UnitTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
