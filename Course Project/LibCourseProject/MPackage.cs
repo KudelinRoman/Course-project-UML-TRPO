0420e67bac6eb7930b2911d69c69304ba0370227
MaximTrapeznikov <maxim1999trapeznikov47@gmail.com>
2019-06-05 23:13:38 +0300

    Создал классы для моделей

---
+++ Course Project/LibCourseProject/MPackage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibCourseProject
+{
+	class MPackage: Model
+	{
+		double _size;
+		object _image;
+		int _coordinates;
+
+		public double Size
+		{
+			get
+			{
+				return _size;
+			}
+			set
+			{
+				_size = value;
+			}
+		}
+
+		public object Image
+		{
+			get
+			{
+				return _image;
+			}
+		}
+
+		public int Coordinates
+		{
+			get
+			{
+				return _coordinates;
+			}
+			set
+			{
+				_coordinates = value;
+			}
+		}
+
+		public MPackage(double size, object image, int coordinates) : base()
+		{
+			Size = size;
+			object Image = image;
+			Coordinates = coordinates;
+		}
+
+	}
+}
