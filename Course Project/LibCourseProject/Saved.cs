d3d842237b6877f56701f96afeeae032c9f2d7c3
KudelinRoman <roman.kuddelin.00@gmail.com>
2019-06-06 01:28:02 +0300

    Класс с листами для сохранения

---
+++ Course Project/LibCourseProject/Saved.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibCourseProject
+{
+	/// <summary>
+	/// В этом классе храняться все листы для сохранения/изменения/удаления моделей
+	/// </summary>
+	public class Saved
+	{
+		public List<CComponent> cComponents = new List<CComponent>();
+		public List<CDependence> cDependences = new List<CDependence>();
+		public List<CInterface> cInterfaces = new List<CInterface>();
+		public List<CLimitOfTwoElements> CLimitOfTwoElements = new List<CLimitOfTwoElements>();
+		public List<CNote> cNotes = new List<CNote>();
+		public List<CPackage> cPackages = new List<CPackage>();
+		public List<CRestriction> cRestrictions = new List<CRestriction>();
+		public List<CRestrictionOr> cRestrictionsOr = new List<CRestrictionOr>();
+		public List<CUnit> cUnits = new List<CUnit>();
+	}
+}
